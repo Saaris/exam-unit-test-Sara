4222f3266ea24db497485cce879e410da0e54c30
Sara Serti <saaris1985@gmail.com>
2025-05-30 12:27:15 +0200

    reindent lines

--- src/tests/validation.test.js
+++ src/tests/validation.test.js
@@ -14,46 +14,47 @@ const exampleCartObject = {
 
 // Group tests using "describe"
 describe('Validering för cart och produkt', () => {
-
+	
 	test('Om isProduct är en giltig produkt returnera true', () => {
 		const validProduct = exampleProduct
 		const expected = true
 		const actual = isProduct(validProduct)
 		expect (actual).toBe(expected)
 	})
-
-	 const invalidProducts = [
-        {}, 
-        { id: 1001, name: 123, price: 500 },
-        { id: null, name: 'Badanka', price: 500 }, 
-        { id: 1001, name: 'Badanka', price: '500' },
-        { id: '1001', name: 'Badanka', price: 500 }, 
-    ]
-
-    test.each(invalidProducts)('isProduct returnerar false för ogiltigt produkt-objekt: %p', (input) => {
+	
+	const invalidProducts = 
+	[
+		{}, 
+		{ id: 1001, name: 123, price: 500 },
+		{ id: null, name: 'Badanka', price: 500 }, 
+		{ id: 1001, name: 'Badanka', price: '500' },
+		{ id: '1001', name: 'Badanka', price: 500 }, 
+	]
+	
+	test.each(invalidProducts)('isProduct returnerar false för ogiltigt produkt-objekt: %p', (input) => {
 		const expected = false
 		const actual = isProduct(input)
 		expect(actual).toBe(expected)
-    })
+	})
 	
-
+	
 	test('isCartItem returnerar true för ett giltigt cart objekt', () => {
 		const expected = true
 		const actual =  isCartItem(exampleCartObject)
 		expect(actual).toBe(expected)
 		
 	})
- 
-		const invalidCartObjects = 
-			 [
-				false,
-				null,
-				{},
-				{ id: 2001, amount: 1, item: null }, 
-				{ id: "2001", amount: 1, item: exampleProduct },
-				{ id: 2001, amount: "1", item: exampleProduct }, 
-			]
-
+	
+	const invalidCartObjects = 
+	[
+		false,
+		null,
+		{},
+		{ id: 2001, amount: 1, item: null }, 
+		{ id: "2001", amount: 1, item: exampleProduct },
+		{ id: 2001, amount: "1", item: exampleProduct }, 
+	]
+	
 	test.each(invalidCartObjects)('isCartItem returnerar false för ogiltigt cart-objekt: %p', (input) => {
 		const expected = false
 		const actual = isCartItem(input)
